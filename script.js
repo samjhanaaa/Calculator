f14f7a08f330c98a942567bf5cf67c51bdab9842
Samjhana Neupane <samjhanaa.neupane@gmail.com>
2024-08-31 23:18:49 +0545

    its done.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const display = document.getElementById("display-result");
+
 function displayValue(value) {
-    document.getElementById("display-result").innerHTML = value;
+    display.textContent += value;
+}
+
+function findAnswer() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch (error) {
+        display.textContent = "Your values are sus";
+    }
+}
+
+function clearDisplay() {
+    display.textContent = '';
+}
+
+function deleteLast() {
+    display.textContent = display.textContent.slice(0, -1);
 }
